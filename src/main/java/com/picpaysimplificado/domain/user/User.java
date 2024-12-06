ef20d4f4563fe2b68343bf06908f7830de72b785
Wanderson Gonçalves <wandersongsoares2001@gmail.com>
2024-12-06 16:51:11 -0300

    Feat : UserService

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,10 +1,7 @@
 package com.picpaysimplificado.domain.user;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
--- src/main/java/com/picpaysimplificado/service/UserService.java
+++ src/main/java/com/picpaysimplificado/service/UserService.java
@@ -4,7 +4,7 @@ package com.picpaysimplificado.service;
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.domain.user.UserType;
 import com.picpaysimplificado.repositories.UserRepository;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
@@ -14,18 +14,22 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public void validateTransaction(User sender, BigDecimal amount){
-        if(sender.getUserType()== UserType.COMMON){
-            throw new IllegalArgumentException("Usuário do tipo Lojista não está autorizado a realizar transação");
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception{
+        if(sender.getUserType()== UserType.MERCHANT){
+            throw new Exception("Usuário do tipo Lojista não está autorizado a realizar transação");
         }
 
         if(sender.getBalance().compareTo(amount)<0){
-            throw new IllegalArgumentException("Saldo insuficiente");
+            throw new Exception("Saldo insuficiente");
         }
 
     }
 
-    public User finUserById(Long id) throws Exception {
-        return this.repository.findUserById(id).orElseThrow(()->new IllegalArgumentException("Usuário não encontrado"));
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(()->new Exception("Usuário não encontrado"));
+    }
+
+    public void saverUser(User user){
+        this.repository.save(user);
     }
 }
